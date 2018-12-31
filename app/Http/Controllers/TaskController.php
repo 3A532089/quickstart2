a0a789f941835676b47e3991d8fd113436590811
3A532089 <likebestupid@gmail.com>
2018-12-31 22:12:40 +0800

    驗證方法定義及錯誤訊息

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,4 +15,10 @@ class TaskController extends Controller
     {
         return view('tasks.index');
     }
+
+    public function store(Request $request)
+{
+    $this->validate($request, [
+        'name' => 'required|max:255',
+    ]);
 }
